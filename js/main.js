1d41ede2a174f598f4f0daf7a8115c06449df917
techchic <dorcas.dorryadhiambo@gmail.com>
2014-02-18 17:00:21 +0300

    event listner

---
+++ js/main.js
@@ -0,0 +1,19 @@
+alert ("Thought block");
+/*var message = false;
+    if (message==="undified");{
+    alert("True"+ message)
+}  
+  else{
+    alert ("False!+" message)
+
+}var button =document.getElementById('my-button')
+    console.log ("button is :" + button)
+button.addEventListener('click', fuction(e){
+    alert("button was clicked")
+});**/
+
+$("#my-button").on("click"
+      function(){
+      alert("you clicked")
+    })
+
